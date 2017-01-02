2a88bc87df1719e99e2d9418b3cb3b59cd1bee1a
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-01-01 22:38:13 -0200

    don't minify css now

--- gulpfile.js
+++ gulpfile.js
@@ -22,12 +22,12 @@
             .pipe(sass())
             .on('error', sass.logError)
             .pipe(gulp.dest('./www/css/'))
-            .pipe(minifyCss({
-                keepSpecialComments: 0
-            }))
-            .pipe(rename({
-                extname: '.min.css'
-            }))
+            // .pipe(minifyCss({
+            //     keepSpecialComments: 0
+            // }))
+            // .pipe(rename({
+            //     extname: '.min.css'
+            // }))
             .pipe(gulp.dest('./www/css/'))
             .on('end', done);
     });
