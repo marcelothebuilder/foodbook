76b926c7a9766ffd8416e53beb7ba1cba366d595
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-01-01 22:38:31 -0200

    remove dead code

--- gulpfile.js
+++ gulpfile.js
@@ -22,13 +22,6 @@
             .pipe(sass())
             .on('error', sass.logError)
             .pipe(gulp.dest('./www/css/'))
-            // .pipe(minifyCss({
-            //     keepSpecialComments: 0
-            // }))
-            // .pipe(rename({
-            //     extname: '.min.css'
-            // }))
-            .pipe(gulp.dest('./www/css/'))
             .on('end', done);
     });
 
