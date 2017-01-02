8dd7497b1f155a8015f0d6e32a77200eab9f25e7
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2017-01-01 22:13:59 -0200

    corrected identifier typo documento->document

--- www/js/app-bootstrap.js
+++ www/js/app-bootstrap.js
@@ -2,7 +2,7 @@
     'use strict';
     angular.element(document).ready(function() {
         if (window.cordova) {
-            documento.addEventListener('deviceready', function() {
+            document.addEventListener('deviceready', function() {
                 angular.bootstrap(document.body, ['io.github.marcelothebuilder.foodbook']);
             }, false);
         } else {
